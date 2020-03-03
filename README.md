9926a4058d99f4819cab688d0aa676e00d2063cb
Mathias Buus <mathiasbuus@gmail.com>
2020-03-03 21:01:41 +0100

    rewrote again

--- README.md
+++ README.md
@@ -11,19 +11,14 @@ npm install @hyperdivision/eth-transaction-tail
 ``` js
 const Tail = require('eth-transaction-tail')
 
-const tail = new Tail(rpcUrl, {
+const tail = new Tail(ipcUrl, {
   confirmations: 10, // require this many confirmations
   depositFactory: '0x...',
-  async isDepositDeployed (addr, blockNumber, txIndex) {
-    // optional! return true if deposit contract is deployed at this addr
-    // if not provided, internally the indexed event is used
-    return true // or false
-  },
   async depositDeployed (event) {
     // deposit is deployed
   },
-  async filter (addr, type, erc20address) {
-    return isInterestingAddress(addr)
+  async filter (toAddr, erc20address) {
+    return isInterestingAddress(toAddr)
   },
   async transaction (transaction) {
     console.log('found this transaction', transaction)
@@ -37,13 +32,13 @@ const tail = new Tail(rpcUrl, {
   async checkpoint (since) {
     // store this since so you can restart from here
   },
-  since: 'now' // or a seq
+  since: seq
 })
 
 // tail.index is the current block index
 await tail.start() // start tailing, will throw if an error is hit
 
-const head = tail.head({
+const head = await tail.head({
   transaction (transaction) {
     console.log('transaction with 0 confirms', transaction)
   }
--- index.js
+++ index.js
@@ -1,216 +1,220 @@
-const tailBlocks = require('./lib/blocks')
-const Web3 = require('web3')
-const hashlru = require('hashlru')
+const BlockQueue = require('./lib/queue')
+const NanoETH = require('nanoeth/ipc')
 const events = require('./lib/events')
 
-const TO = Symbol('to address')
-const FROM = Symbol('from address')
-
 module.exports = class Tail {
-  constructor (wsUrl, opts = {}) {
+  constructor (ipc, opts) {
     this.erc20 = opts.erc20 !== false
-    this.deployCache = makeCache(opts.deployCache)
     this.depositFactory = [].concat(opts.depositFactory || [])
-    this.web3 = opts.web3 || new Web3(new Web3.providers.WebsocketProvider(wsUrl))
-    this.confirmations = typeof opts.confirmations === 'number' ? opts.confirmations : 12
-    this.filter = opts.filter || (() => true)
-    this.ondepositdeployed = opts.depositDeployed || (() => {})
-    this.ondeposit = opts.deposit || (() => {})
-    this.ontransaction = opts.transaction || (() => {})
-    this.oncheckpoint = opts.checkpoint || (() => {})
-    this.onerc20 = opts.erc20 || (() => {})
-    this.onblock = opts.block || (() => {})
-    this.onfork = opts.fork || (() => {})
-    this.since = opts.since || 0
-    this.minSince = opts.minSince || 0
-    this.maxParallel = 1
-    this.hardMaxParallel = opts.maxParallel || 64
-    this.stopped = false
-    this.running = null
-    this.topics = [events.DEPOSIT_FACTORY_DEPLOYED.id, events.DEPOSIT_FORWARDED.id].concat(this.erc20 ? events.ERC20_TRANSFER.id : [])
+    this.started = null
+    this.filter = opts.filter || noop
+    this.ontransaction = opts.transaction || noop
+    this.ondepositdeployed = opts.depositDeployed || noop
+    this.ondeposit = opts.deposit || noop
+    this.oncheckpoint = opts.checkpoint || noop
+    this.onblock = opts.block || noop
+    this.ids = [
+      events.DEPOSIT_FACTORY_DEPLOYED.id,
+      events.DEPOSIT_FORWARDED.id
+    ].concat(this.erc20 ? events.ERC20_TRANSFER.id : [])
 
-    if (opts.isDepositDeployed) this.isDepositDeployed = opts.isDepositDeployed
+    this.eth = opts.eth || new NanoETH(ipc)
+    this.queue = new BlockQueue(this.eth, {
+      ...opts,
+      get: this.get.bind(this)
+    })
+
+    this.getBlockByNumber = this._retry((seq, tx) => this.eth.getBlockByNumber(seq, tx))
+    this.getLogs = this._retry((opts) => this.eth.getLogs(opts))
+    this.getTransactionReceipt = this._retry((hash) => this.eth.getTransactionReceipt(hash))
   }
 
-  static TO = TO
-  static FROM = FROM
-
-  async start (since = this.since) {
-    if (this.running) throw new Error('Already started')
-
-    if (since === 'now' || since === 'latest') {
-      since = this.since = Math.max(this.minSince, await this.web3.eth.getBlockNumber())
-    }
-
-    const status = tailBlocks(this.web3, since, this.confirmations, this._onblock.bind(this), this.onfork.bind(this))
-    const cleanup = () => { this.running = null }
-    this.stopped = false
-    this.running = status
-    status.promise.then(cleanup).catch(cleanup)
-    await status.promise
+  get since () {
+    return this.queue.tail
   }
 
-  stop () {
-    if (!this.running) return Promise.resolve()
-    this.stopped = true
-    this.running.stop()
-    return this.running.promise
+  get blockHeight () {
+    return this.queue.blockHeight
   }
 
-  head (opts) {
+  async head (opts) {
+    const now = await this.now()
+    const min = this.queue.tail + this.queue.confirmations
+
     return new Tail(null, {
-      web3: this.web3,
-      deployCache: false,
-      confirmations: 0,
-      since: 'now',
-      minSince: typeof this.since === 'number' ? this.since + this.confirmations : 0,
+      eth: this.eth,
       depositFactory: this.depositFactory,
       filter: this.filter,
+      erc20: this.erc20,
+      since: Math.max(min, now),
+      confirmations: 0,
       ...opts
     })
   }
 
-  isDepositDeployedCached (addr, blockNumber) {
-    if (this.deployCache) {
-      const deployed = this.deployCache.get(addr.toLowerCase())
-      if (deployed === false) return Promise.resolve(false)
-      if (deployed === true) return Promise.resolve(true)
-    }
-    return this.isDepositDeployed(addr, blockNumber)
-  }
+  async now () {
+    let prev = null
 
-  isDepositDeployed (addr, blockNumber) {
-    return this.isDepositDeployedFromChain(addr, blockNumber)
-  }
+    while (true) {
+      const height = await this.eth.blockNumber()
+      if (height === prev) {
+        await this.queue.blocking.wait(500)
+        continue
+      }
 
-  async isDepositDeployedFromChain (addr, blockNumber) {
-    const code = await this.web3.eth.getCode(addr, blockNumber)
-    return code !== '0x'
-  }
+      prev = height
 
-  onlog (log, e, tx, blk, confirmations, deployStatus) {
-    const blockNumber = blk.number
-    const transactionIndex = tx.transactionIndex
-    const logIndex = log.logIndex
-    const transactionHash = tx.hash
-
-    if (e.name === 'DEPOSIT_FACTORY_DEPLOYED' && eqList(log.address, this.depositFactory)) {
-      deployStatus.set(e.contractAddress.toLowerCase(), Promise.resolve(true))
-      return this.ondepositdeployed({ contractAddress: e.contractAddress, transactionHash, blockNumber, transactionIndex, logIndex }, confirmations, tx, blk, log)
-    }
-
-    if (e.name === 'DEPOSIT_FORWARDED') {
-      return this.ondeposit({ from: log.address, to: e.to, amount: e.amount, transactionHash, blockNumber, transactionIndex, logIndex }, confirmations, tx, blk, log)
-    }
-
-    if (e.name === 'ERC20_TRANSFER') {
-      return this.onerc20({ from: e.from, to: e.to, amount: e.amount, token: log.address, transactionHash, blockNumber, transactionIndex, logIndex }, confirmations, tx, blk, log)
+      const { timestamp, number } = await this.eth.getBlockByNumber(height)
+      const ms = Number(timestamp) * 1000
+      if (ms >= Date.now() || (ms - Date.now()) < 10000) return Number(number)
+      const delta = (Date.now() - ms) - 60000
+      await this.queue.blocking.wait(Math.max(500, delta))
     }
   }
 
-  async _onblock (blk, confirmations) {
-    await this.onblock(blk)
-
-    const logs = (await this.web3.eth.getPastLogs({
-      fromBlock: blk.number,
-      toBlock: blk.number,
-      topics: [this.topics]
-    })).sort(sortLogs)
+  async get (seq) {
+    const [block, logs] = await Promise.all([
+      this.getBlockByNumber(hex(seq), true),
+      this.getLogs({ fromBlock: hex(seq), toBlock: hex(seq), topics: [this.ids] })
+    ])
 
     const queue = []
-    const deployStatus = new Map()
+    const isContract = new Set()
 
-    if (this.stopped) return
+    let l = 0
 
-    try {
-      let l = 0
-      let parallel = 0
+    for (let i = 0; i < block.transactions.length; i++) {
+      const tx = block.transactions[i]
 
-      for (let i = 0; i < blk.transactions.length; i++) {
-        const tx = blk.transactions[i]
+      let receipt = null
+      let prev = -1
 
-        while (l < logs.length && logs[l].transactionIndex === i) {
-          const log = logs[l++]
-          const e = events.decode(log)
-          if (!e) continue
+      while (l < logs.length && Number(logs[l].transactionIndex) === i) {
+        const log = logs[l++]
 
-          if (e.name === 'ERC20_TRANSFER') {
-            if (!(await this.filter(e.to, TO, log.address)) && !(await this.filter(e.from, FROM, log.address))) {
-              continue
-            }
-          }
-          if (e.name === 'DEPOSIT_FACTORY_DEPLOYED' && !eqList(log.address, this.depositFactory)) {
+        // Couldn't see the ordering mentioned in the logs,
+        // but everything seems to be ordered, so lets just assert that also
+        const logIndex = Number(log.logIndex)
+        if (logIndex <= prev) throw new Error('Invalid log order')
+        prev = logIndex
+
+        const e = events.decode(log)
+        if (!e) continue
+
+        const addr = log.address
+
+        if (e.name === 'ERC20_TRANSFER') {
+          if (!(await this.filter(e.to, addr))) continue
+        }
+
+        if (e.name === 'DEPOSIT_FORWARDED') {
+          if (!(await this.filter(addr, addr))) continue
+        }
+
+        if (e.name === 'DEPOSIT_FACTORY_DEPLOYED') {
+          if (!eqList(addr, this.depositFactory)) continue
+        }
+
+        isContract.add(addr)
+        if (!receipt) receipt = this.getTransactionReceipt(tx.hash)
+        queue.push({ receipt, status: false, tx, log, event: e })
+      }
+
+      if (isContract.has(tx.to)) continue
+      if (!(await this.filter(tx.to, null))) continue
+
+      if (!receipt) receipt = this.getTransactionReceipt(tx.hash)
+      queue.push({ receipt, status: false, tx, log: null, event: null })
+    }
+
+    await Promise.all(queue.map(async (q) => {
+      q.status = (await q.receipt).status === '0x1'
+    }))
+
+    return { block, queue }
+  }
+
+  start () {
+    if (this.started) return this.started
+
+    this.started = Promise.all([
+      this.queue.start(),
+      this.loop()
+    ])
+
+    return this.started
+  }
+
+  stop (gracefully = false) {
+    if (gracefully) this.eth.end()
+    else this.eth.destroy()
+    return this.queue.destroy()
+  }
+
+  _retry (fn) {
+    const eth = this.eth
+
+    return async function (...args) {
+      let error = null
+
+      for (let i = 0; i < 30; i++) {
+        if (i) await this.queue.blocking.wait(1000)
+
+        try {
+          return await fn(...args)
+        } catch (err) {
+          error = err
+        }
+
+        if (eth.destroyed) break
+      }
+
+      throw error
+    }
+  }
+
+  async loop () {
+    while (!this.queue.destroyed) {
+      const { block, queue } = await this.queue.shift()
+
+      const blockNumber = Number(block.number)
+      const confirmations = this.queue.blockHeight - blockNumber
+
+      await this.onblock(block, confirmations)
+
+      for (const { status, tx, log, event } of queue) {
+        if (!status) continue
+        if (event) {
+          if (event.name === 'DEPOSIT_FACTORY_DEPLOYED') {
+            await this.ondepositdeployed({ contractAddress: event.contractAddress }, tx, confirmations, block, log)
             continue
           }
 
-          // TODO: shortcircuit the DEPOSIT_FORWARDED event also here for more speed
+          if (event.name === 'DEPOSIT_FORWARDED') {
+            await this.ondeposit({ from: log.address, to: event.to, amount: event.amount }, tx, confirmations, block, log)
+            continue
+          }
 
-          const receipt = this.web3.eth.getTransactionReceipt(tx.hash)
-
-          queue.push({
-            tx,
-            checkDeployed: false,
-            receipt,
-            op: () => this.onlog(log, e, tx, blk, confirmations, deployStatus)
-          })
-
-          if (++parallel > this.maxParallel) await receipt
+          if (event.name === 'ERC20_TRANSFER') {
+            await this.onerc20({ from: event.from, to: event.to, amount: event.amount, token: log.address }, tx, confirmations, block, log)
+            continue
+          }
+        } else {
+          await this.ontransaction(tx, confirmations, block)
         }
-
-        if (this.stopped) return finalise()
-        if (!(await this.filter(tx.to, TO, null)) && !(await this.filter(tx.from, FROM, null))) continue
-        if (this.stopped) return finalise()
-
-        if (tx.to && !deployStatus.has(tx.to.toLowerCase())) {
-          const p = this.isDepositDeployedCached(tx.to, blk.number - 1)
-          deployStatus.set(tx.to.toLowerCase(), p)
-          if (++parallel > this.maxParallel) await p
-        }
-
-        const receipt = this.web3.eth.getTransactionReceipt(tx.hash)
-
-        queue.push({
-          tx,
-          checkDeployed: !!tx.to,
-          receipt,
-          op: () => this.ontransaction(tx, confirmations, blk)
-        })
-
-        if (++parallel > this.maxParallel) await receipt
       }
 
-      for (const { tx, checkDeployed, receipt, op } of queue) {
-        if (checkDeployed && await deployStatus.get(tx.to.toLowerCase())) continue
-        if (!(await receipt).status) continue
-        await op()
-        if (this.maxParallel < this.hardMaxParallel) this.maxParallel++
-      }
-    } catch (err) {
-      await finalise(err)
-      throw err
-    }
-
-    if (this.deployCache) {
-      for (const [k, v] of deployStatus) {
-        this.deployCache.set(k, await v)
-      }
-    }
-
-    await this.oncheckpoint(blk.number + 1, confirmations, blk)
-
-    async function finalise () {
-      const list = []
-      for (const { receipt } of queue) list.push(receipt)
-      for (const v of deployStatus.values()) list.push(v)
-      await Promise.allSettled(list)
+      await this.oncheckpoint(blockNumber + 1)
     }
   }
 }
 
-function sortLogs (a, b) {
-  if (a.blockNumber !== b.blockNumber) return a.blockNumber - b.blockNumber
-  if (a.transactionIndex !== b.transactionIndex) return a.transactionIndex - b.transactionIndex
-  return a.logIndex - b.logIndex
+function noop () {
+  return true
+}
+
+function hex (n) {
+  return '0x' + n.toString(16)
 }
 
 function eqList (a, list) {
@@ -223,10 +227,4 @@ function eqList (a, list) {
 function eq (a, b) {
   if (!a || !b) return false
   return a.toLowerCase() === b.toLowerCase()
-}
-
-function makeCache (size) {
-  if (size === false || size === 0) return null
-  if (size && typeof size !== 'number') return size
-  return hashlru(typeof size === 'number' ? size : 1024)
 }
